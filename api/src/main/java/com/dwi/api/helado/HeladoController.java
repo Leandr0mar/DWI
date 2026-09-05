419a5acc595542e311a6a8dbb75ea1e8fe0839ab
Leandro <leandro.falcon.99@outlook.com>
2026-09-04 22:32:02 -0500

    Primer controller creado y testeado

--- api/src/main/java/com/dwi/api/helado/HeladoController.java
+++ api/src/main/java/com/dwi/api/helado/HeladoController.java
@@ -1,5 +1,25 @@
 package com.dwi.api.helado;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping ("/api/helados")
 public class HeladoController {
+    
+    private final HeladoService heladoService;
+
+    public HeladoController(HeladoService heladoService) {
+        this.heladoService = heladoService;
+    }
+    
+    @GetMapping("/listar")
+    public List<Helado> ListarHelados() {
+        return heladoService.listarTodos();
+    }
     
 }
