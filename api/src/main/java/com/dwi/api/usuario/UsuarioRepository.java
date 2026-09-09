568839e53d4256b01bc5b880eadb632135f022d5
Leandro <leandro.falcon.99@outlook.com>
2026-09-09 18:13:08 -0500

    repoistory de usuario

--- api/src/main/java/com/dwi/api/usuario/UsuarioRepository.java
+++ api/src/main/java/com/dwi/api/usuario/UsuarioRepository.java
@@ -3,5 +3,5 @@ package com.dwi.api.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-sdadsadsadas
+
 }
